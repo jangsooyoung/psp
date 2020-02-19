0d2a2367166be5b3754dfd35e1828acc18f82105
j <jangsooy@gmail.com>
2020-02-19 12:50:17 +0900

    add declare stamt

--- gen.py
+++ gen.py
@@ -10,6 +10,7 @@ class Gen:
     end_mark = 3
     block_begin = 4
     block_end = 5
+    begin_declare = 6
     line=0
     tabsz=cfg.tab
     space="                                                                                           "
@@ -26,7 +27,9 @@ class Gen:
             return 5, self.block_begin
         if c + 6 <= self.source_len and self.source[c:c+5] == '<%}%>':
             print('self.block_end')
-            return 6, self.block_end
+            return 5, self.block_end
+        if c + 3 <= self.source_len and self.source[c:c+3] == '<%!':
+            return 3, self.begin_declare
         if c + 2 <= self.source_len and self.source[c:c+2] == '<%':
             return 2, self.begin_source
         return 1, self.source[c]
@@ -48,47 +51,58 @@ class Gen:
         self.source = self.fileToString(psp_name)
         self.source_len = len(self.source)
 
+        declare_list =[]
+        source_list = []
+        c = 0
+        source_list.append("# -*- coding: utf-8 -*-\n")
+        source_list.append("from io import BytesIO\n")
+        source_list.append("import sys, os\n")
+        source_list.append("from pspconfig import PspConfig\n")
+        source_list.append("def call_psp_(http_handler, args):\n")
+        source_list.append("    _psp_out_ = BytesIO()\n")
+        while True:
+            sz, tok = self.seeToken(c)
+            if sz == 0:
+                break;
+            elif tok == self.block_begin:
+                c += sz
+                self.tabsz += 4
+            elif tok == self.block_end:
+                c += sz
+                self.tabsz -= 4
+            elif tok == self.begin_declare:
+                c += sz
+                n = self.findKeyword2(c, "%>")
+                declare_list.append(''.join(self.source[c:n]))
+                c = (n + 2)
+            elif tok == self.begin_source:
+                c += sz
+                n = self.findKeyword2(c, "%>")
+                source_list.append(self.tab()+''.join(self.source[c:n]).replace('\n', '\n'+self.tab()))
+                c = (n + 2)
+                source_list.append('\n')
+            elif tok == self.begin_var:
+                c += sz
+                n = self.findKeyword2(c, "%>")
+                source_list.append(self.tab()+"_psp_out_.write(" + ''.join(self.source[c:n]) + ".encode())\n")
+                c = (n + 2)
+            else:
+                # <%, line feed 나올떄 까지  읽기
+                n, is_line_end = self.findHtmlEnd(c)
+                stmt = ''.join(self.source[c: n]).replace('\"', '\\\"')
+                if is_line_end:
+                    source_list.append(self.tab()+"_psp_out_.write(\"" + stmt + "\\n\".encode())\n")
+                    c = n + 1
+                else :
+                    source_list.append(self.tab()+"_psp_out_.write(\"" + stmt + "\".encode())\n")
+                    c = n
+        source_list.append(self.tab()+"return _psp_out_\n")
+
         with open(py_name, 'w') as gen:
-            #self.source = list(text)
-            c = 0
-            gen.write("# -*- coding: utf-8 -*-\n")
-            gen.write("from io import BytesIO\n")
-            gen.write("import sys, os\n")
-            gen.write("from pspconfig import PspConfig\n")
-            gen.write("def call_psp_(http_handler, args):\n")
-            gen.write("    _psp_out_ = BytesIO()\n")
-            while True:
-                sz, tok = self.seeToken(c)
-                if sz == 0:
-                    break;
-                elif tok == self.block_begin:
-                    c += sz
-                    self.tabsz += 4
-                elif tok == self.block_end:
-                    c += sz
-                    self.tabsz -= 4
-                elif tok == self.begin_source:
-                    c += sz
-                    n = self.findKeyword2(c, "%>")
-                    gen.write(self.tab()+''.join(self.source[c:n]).replace('\n', '\n'+self.tab()))
-                    c = (n + 2)
-                    gen.write('\n')
-                elif tok == self.begin_var:
-                    c += sz
-                    n = self.findKeyword2(c, "%>")
-                    gen.write(self.tab()+"_psp_out_.write(" + ''.join(self.source[c:n]) + ".encode())\n")
-                    c = (n + 2)
-                else:
-                    # <%, line feed 나올떄 까지  읽기
-                    n, is_line_end = self.findHtmlEnd(c)
-                    stmt = ''.join(self.source[c: n]).replace('\"', '\\\"')
-                    if is_line_end:
-                        gen.write(self.tab()+"_psp_out_.write(\"" + stmt + "\\n\".encode())\n")
-                        c = n + 1
-                    else :
-                        gen.write(self.tab()+"_psp_out_.write(\"" + stmt + "\".encode())\n")
-                        c = n
-            gen.write(self.tab()+"return _psp_out_\n")
+            for l in declare_list:
+                gen.write(l)
+            for l in source_list:
+                gen.write(l)
         print("generate {}".format(py_name))
     def tab(self):
         return self.space[0:self.tabsz]
